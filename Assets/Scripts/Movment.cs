630c5ca1ce1d49b0fcd8b2159368cab3502457e6
Ria8651 <bk.wood@vodafone.co.nz>
2020-11-27 16:38:49 +1300

    Added comments to code

--- Assets/Scripts/Movment.cs
+++ Assets/Scripts/Movment.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
+// enum for player states
 public enum State {
     Normal,
     Flying,
@@ -8,12 +9,16 @@ public enum State {
     InSeat
 }
 
+// this component reqires accsess to a gravity component
 [RequireComponent(typeof(GravityComponent))]
 public class Movment : MonoBehaviour {
 
+    // instance of player state enum
     [SerializeField] State state;
+    // refernce to camera script
     public new MouseLook camera;
 
+    // variables that control player movment in the normal player state
     [Header("Normal")]
     public float speed = 6;
     public float sprintSpeedMultiplyer = 1;
@@ -22,31 +27,41 @@ public class Movment : MonoBehaviour {
     public float midAirControlDivisor = 6;
     public float groundForce = 1;
 
+    // variables that control player movment when floating in space
     [Header("Floating")]
     public float floatingSpeed = 0.05f;
 
+    // public variables that dont show in the inspector (so other scripts can accsess them)
     [HideInInspector] public Vector3 currentGravityDirection;
     [HideInInspector] public Rigidbody rb;
     [HideInInspector] public VehicleSeat seat;
 
+    // runtime reference to the gravity component required on line 13
     GravityComponent gravityComponent;
+    // private variables to store the players current state
     Vector3 groundSpeed;
     Vector3 groundRotation;
     bool grounded;
     bool jump;
     bool sprinting;
 
+    // start method called when the game starts
     void Start() {
+        // getting refrences too required components
         rb = GetComponent<Rigidbody>();
         gravityComponent = GetComponent<GravityComponent>();
 
+        // initilising variables
         camera.character = this;
         currentGravityDirection = gravityComponent.gravityDirection;
 
+        // initilising state
         SetState(state);
     }
 
+    // update method called every frame
     void Update() {
+        // input is controlled in update instead of fixed update to avoid input loss
         if (state == State.Normal) {
             if (InputSystem.Instance.GetInputDown(InputType.Up)) {
                 jump = true;
@@ -59,6 +74,7 @@ public class Movment : MonoBehaviour {
             }
         }
 
+        // flight feature for debuging
         if (Input.GetKeyDown(KeyCode.T)) {
             if (state != State.Flying && state != State.InSeat) {
                 SetState(State.Flying);
@@ -68,124 +84,188 @@ public class Movment : MonoBehaviour {
         }
     }
 
+    // update method called 50 times a second - used for pysics
     void FixedUpdate() {
+        // update player state
         if (gravityComponent.IsInGravityFeild() && state == State.Floating) {
             SetState(State.Normal);
         } else if (!gravityComponent.IsInGravityFeild() && state == State.Normal) {
             SetState(State.Floating);
         }
 
-        if (state == State.Normal) {
-            Vector2 movment = InputSystem.Instance.GetAxis(InputType.Movment);
-            movment = movment.normalized * speed;
+        // switch for each player state
+        switch (state) {
+            // if player is in normal state
+            case (State.Normal):
+                // get input from input system and scale it by speed
+                Vector2 movment = InputSystem.Instance.GetAxis(InputType.Movment);
+                movment = movment.normalized * speed;
 
-            Vector3 targetVelocity = transform.forward * movment.y + transform.right * movment.x;
+                // caclulates target velocity based on input
+                Vector3 targetVelocity = transform.forward * movment.y + transform.right * movment.x;
 
-            Vector3 combinedGroundSpeed = groundSpeed; // somehow calculate velocity
-            targetVelocity += Vector3.ProjectOnPlane(combinedGroundSpeed, transform.up);
+                // get speed of the ground to add to target velocity
+                Vector3 combinedGroundSpeed = groundSpeed; // somehow add rotational velocity
+                targetVelocity += Vector3.ProjectOnPlane(combinedGroundSpeed, transform.up);
 
-            Vector3 localVelocity = transform.InverseTransformDirection(rb.velocity);
-            Vector3 currentVelocity = transform.forward * localVelocity.z + transform.right * localVelocity.x;
+                // turn velocity from global space to local space
+                Vector3 localVelocity = transform.InverseTransformDirection(rb.velocity);
+                // remove vertical component of velocity
+                Vector3 currentVelocity = transform.forward * localVelocity.z + transform.right * localVelocity.x;
 
-            // Vector3 localGroundSpeed = transform.InverseTransformDirection(groundSpeed);
-            // rb.AddForce(localGroundSpeed.z * transform.up, ForceMode.VelocityChange);
+                // Vector3 localGroundSpeed = transform.InverseTransformDirection(groundSpeed);
+                // rb.AddForce(localGroundSpeed.z * transform.up, ForceMode.VelocityChange);
 
-            if (sprinting && movment.y > 0) {
-                targetVelocity += transform.forward * speed * (sprintSpeedMultiplyer - 1);
-            }
-
-            if (grounded) {
-                if (jump) {
-                    rb.AddForce(transform.up * jumpPower);
-
-                    jump = false;
+                // increase speed if walking forwad and sprinting
+                if (sprinting && movment.y > 0) {
+                    targetVelocity += transform.forward * speed * (sprintSpeedMultiplyer - 1);
                 }
 
-                rb.velocity += (targetVelocity - currentVelocity) * acceleration;
-            } else {
-                if (jump) {
-                    jump = false;
+                // jump if player is grounded
+                if (grounded) {
+                    if (jump) {
+                        rb.AddForce(transform.up * jumpPower);
+
+                        jump = false;
+                    }
+
+                    // change player velocity based on the difference of targetvelocity and currentvelocity
+                    rb.velocity += (targetVelocity - currentVelocity) * acceleration;
+                } else {
+                    // acknowledge input from update loop without jumping
+                    if (jump) {
+                        jump = false;
+                    }
+
+                    // change player velocity slow when in mid air
+                    rb.velocity += (targetVelocity - currentVelocity) * acceleration / midAirControlDivisor;
                 }
 
-                rb.velocity += (targetVelocity - currentVelocity) * acceleration / midAirControlDivisor;
-            }
-            
-            if (localVelocity.y < 0) {
-                rb.AddForce(gravityComponent.gravityDirection * groundForce, ForceMode.VelocityChange);
-            }
+                // make player fall faster on the way down. common techqniqe to make movment feel nicer
+                if (localVelocity.y < 0) {
+                    rb.AddForce(gravityComponent.gravityDirection * groundForce, ForceMode.VelocityChange);
+                }
 
-            if (grounded) {
-                GetComponent<MeshRenderer>().material.color = Color.white;
-            } else {
-                GetComponent<MeshRenderer>().material.color = Color.red;
-            }
+                // change player color for debuging
+                if (grounded) {
+                    GetComponent<MeshRenderer>().material.color = Color.white;
+                } else {
+                    GetComponent<MeshRenderer>().material.color = Color.red;
+                }
 
-            Vector3 difference = gravityComponent.gravityDirection - currentGravityDirection;
-            currentGravityDirection += difference / 10;
-        } else if (state == State.Flying) {
-            Vector3 movment = InputSystem.Instance.GetAxis(InputType.Movment);
-            movment = movment.normalized * speed * 5;
+                // interpolate players gravity vector
+                Vector3 difference = gravityComponent.gravityDirection - currentGravityDirection;
+                currentGravityDirection += difference / 10;
 
-            Vector3 targetVelocity = transform.forward * movment.y + transform.right * movment.x + transform.up * movment.z;
-            Vector3 currentVelocity = rb.velocity;
+                break;
 
-            rb.velocity += (targetVelocity - currentVelocity) * acceleration;
-        } else if (state == State.Floating) {
-            float movment = InputSystem.Instance.GetAxis(InputType.Movment).y;
+            // if player is in floating state
+            case (State.Floating):
+                // get only forwad and back input
+                float floatingInput = InputSystem.Instance.GetAxis(InputType.Movment).y;
 
-            rb.velocity += camera.transform.forward * movment * floatingSpeed;
+                // accelerate player based on forwad and back input
+                rb.velocity += camera.transform.forward * speed * floatingInput;
+
+                break;
+
+            // if player is in flying state
+            case (State.Flying):
+                // get input from input system
+                Vector3 flyingInput = InputSystem.Instance.GetAxis(InputType.Movment);
+                // scale input by speed
+                flyingInput = flyingInput.normalized * speed * 5;
+
+                // calculate target velocity based on input
+                Vector3 flyingTargetVelocity = transform.forward * flyingInput.y + transform.right * flyingInput.x + transform.up * flyingInput.z;
+                // get current velocity
+                Vector3 flyingCurrentVelocity = rb.velocity;
+
+                // change player velocity based on the difference of targetvelocity and currentvelocity
+                rb.velocity += (flyingTargetVelocity - flyingCurrentVelocity) * acceleration;
+
+                break;
         }
     }
 
+    // function for updating the players state
     public void SetState(State _state) {
-        if (_state == State.Normal) {
-            rb.constraints = RigidbodyConstraints.FreezeRotation;
-            rb.isKinematic = false;
+        // switch for player states
+        switch (_state) {
+            case (State.Normal):
+                // change rigidbody contraints to let player move properly
+                rb.constraints = RigidbodyConstraints.FreezeRotation;
+                // makes player active so it can colllide and respond
+                rb.isKinematic = false;
 
-            currentGravityDirection = -transform.up;
-            // camera.cameraRotation = transform.forward;
+                // reset current gravity direction
+                currentGravityDirection = -transform.up;
+                // camera.cameraRotation = transform.forward;
 
-            gravityComponent.enabled = true;
-        } else if (_state == State.Floating) {
-            rb.constraints = RigidbodyConstraints.None;
-            rb.isKinematic = false;
+                // renables gravity
+                gravityComponent.enabled = true;
 
-            gravityComponent.enabled = true;
-        } else if (_state == State.InSeat) {
-            camera.GetInSeat();
+                break;
 
-            rb.isKinematic = true;
+                // the other cases are the oppisite or simmillar
 
-            gravityComponent.enabled = false;
-        } else if (_state == State.Flying) {
-            rb.constraints = RigidbodyConstraints.FreezeRotation;
-            rb.isKinematic = false;
+            case (State.Floating):
+                rb.constraints = RigidbodyConstraints.None;
+                rb.isKinematic = false;
 
-            gravityComponent.enabled = false;
+                gravityComponent.enabled = true;
 
-            currentGravityDirection = -transform.up;
-            // camera.cameraRotation = transform.forward;
+                break;
+
+            case (State.InSeat):
+                camera.GetInSeat();
+
+                rb.isKinematic = true;
+
+                gravityComponent.enabled = false;
+
+                break;
+
+            case (State.Flying):
+                rb.constraints = RigidbodyConstraints.FreezeRotation;
+                rb.isKinematic = false;
+
+                gravityComponent.enabled = false;
+
+                currentGravityDirection = -transform.up;
+                // camera.cameraRotation = transform.forward;
+
+                break;
         }
 
+        // to remove duplicate code
         if (_state != State.InSeat) {
             if (state == State.InSeat) {
+                // make sure camera is looking in the same direction
                 transform.LookAt(transform.position + Vector3.ProjectOnPlane(camera.transform.forward, transform.up), transform.up);
 
+                // put camera back inside the player
                 camera.ResetTransform();
             }
         }
 
+        // set state to new state
         state = _state;
     }
 
+    // exposing private variable through public method
     public State GetState() {
         return state;
     }
 
+    // method called on fixed update when the ground trigger is intersecting somthing
     void OnTriggerStay(Collider other) {
+        // so you cant walk on gravity sources
         if (other.gameObject.tag != "Gravity Source") {
+            // set grounded to true so rest of the script knows whats happening
             grounded = true;
-            
+
+            // if other object is rigidbody extract their velocity and rotation
             if (other.gameObject.GetComponent<Rigidbody>()) {
                 if (other.gameObject.tag == "VehiclePlayerCollider") {
                     groundSpeed = other.transform.parent.GetComponent<Rigidbody>().velocity;
@@ -195,13 +275,17 @@ public class Movment : MonoBehaviour {
                     groundRotation = other.transform.GetComponent<Rigidbody>().angularVelocity;
                 }
             } else {
+                // if no on ground ground speed is zero
                 groundSpeed = Vector3.zero;
             }
         }
     }
 
+    // method called when the on fixed update when the ground trigger leaves the ground
     void OnTriggerExit(Collider other) {
+        // so you cant walk on gravity sources
         if (other.gameObject.tag != "Gravity Source") {
+            // set grounded to false so rest of the script knows whats happening
             grounded = false;
         }
     }
--- Assets/Scripts/VehicleController.cs
+++ Assets/Scripts/VehicleController.cs
@@ -1,84 +1,118 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
+// enum to store vechcile types
 public enum VehicleType {
     SpaceShip,
     HoverCar,
     Mech
 }
 
+// this component reqires accsess to a gravity component
 [RequireComponent(typeof(GravityComponent))]
 public class VehicleController : MonoBehaviour {
-    
+    // instace of vehicle type enum
     public VehicleType vehicleType;
+    // refrence to collider that the player will interact with
     public Collider playerCollider;
+    // variables that control how the ship moves
     public float speed = 32;
     public float acceleration = 0.02f;
     public float rotationalSpeed = 1.5f;
     public float angularAcceleration = 0.04f;
     public float hoverForce = 13;
+    // refrence to hoverpad location
     public Transform[] hoverpads = new Transform[4];
+    // refrence to center of mass transform
     public Transform centerOfMass;
 
-    [HideInInspector] List<GravityContainer> gravitySources;
+    // refrence to rigidbody avliable to other scripts but not visible in the inspector
     [HideInInspector] public Rigidbody rb;
 
+    // player input sent from the player
     Vector3 movement;
+    // runtime reference to the gravity component required on line 12
     GravityComponent gravityComponent;
 
+    // start method called when the game starts
     void Start() {
+        // getting refrences too required components
         rb = GetComponent<Rigidbody>();
         gravityComponent = GetComponent<GravityComponent>();
 
+        // set rigidbodys center of mass to center of mass empty
         rb.centerOfMass = centerOfMass.localPosition;
 
+        // ignore collition between the ship collider and the player collider
         if (playerCollider != null) {
             Physics.IgnoreCollision(GetComponent<Collider>(), playerCollider);
         }
     }
 
+    // update method called 50 times a second - used for pysics
     void FixedUpdate() {
+        // caclulates target velocity based on input
         Vector3 targetVelocity = transform.forward * movement.y * speed;
+        // get global velocity
         Vector3 currentVelocity = rb.velocity;
         // Vector3 currentVelocity = Vector3.ProjectOnPlane(rb.velocity, transform.up);
+        // change velocity based on the difference of targetvelocity and currentvelocity
         Vector3 difference = (targetVelocity - currentVelocity) * acceleration;
         rb.velocity += difference;
-        
+
+        // calculate target angular velocity
         Vector3 targetAngularVelocity = (transform.up * movement.x + transform.right * -movement.z) * rotationalSpeed;
+        // change angular velocity based on the difference of target angular velocity and current anular velocity
         Vector3 angularDifference = (targetAngularVelocity - rb.angularVelocity) * angularAcceleration;
         rb.angularVelocity += angularDifference;
 
+        // add forces from the hover thrusters if vehcile is a hover car
         if (vehicleType == VehicleType.HoverCar) {
+            // loop over thrusters
             foreach (Transform hoverpad in hoverpads) {
+                // draw ray for debuging
                 Debug.DrawRay(hoverpad.position, -hoverpad.up * 5, Color.green, 0, false);
 
+                // create constant for ray length
                 const float rayLength = 10;
+                // get distance using distance function
                 float distance = GetGroundDistance(hoverpad, rayLength);
 
+                // calculate force using hyperbola to get expeonential growth
                 Vector3 force = transform.up * (1 / (distance + 1)) * hoverForce;
-                
+
+                // add the force at the position of the thruster
                 rb.AddForceAtPosition(force, hoverpad.position);
             }
         }
     }
 
+    // function for getting input forwared from the player
     public void Move(Vector3 movement) {
         this.movement = movement;
     }
 
+    // utillity function for getting the distance to the ground
     float GetGroundDistance(Transform hoverpad, float rayLength) {
+        // caclulate direction thruster is facing
         Vector3 direction = -hoverpad.up * rayLength;
 
+        // array to store all raycast hits
         RaycastHit[] hits;
+        // get raycast hits using pysics sytem
         hits = Physics.RaycastAll(hoverpad.position, direction, rayLength);
 
+        // loop over hits to get the closest one that isnt a gravity source
         float distance = 1000000000;
         for (int i = 0; i < hits.Length; i++) {
+            // check if hit is a gravity source and closer than the current closest hit
             if (hits[i].transform.tag != "Gravity Source" && hits[i].distance < distance) {
+                // set closest hit to hit distance
                 distance = hits[i].distance;
             }
         }
 
+        // return the closest hit that isnt a gravity source
         return distance;
     }
 }
